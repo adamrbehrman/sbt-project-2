269062e8587e9ccde6200f3e953ee28f643cfc36
hadoop <hadoop@Adams-MBP-2.attlocal.net>
2021-09-02 14:08:19 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,21 @@
+
+name := "StocksProject"
+version := "0.1"
+scalaVersion := "2.12.14"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.2" 
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2" 
+
+assemblyJarName in assembly := "StockProject--fatjar-0.1.jar"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+
+enablePlugins(GitVersioning, GitBranchPrompt)
+
+git.baseVersion := "1.0"
+
+
+
---
+++ src/main/scala/stocksproject/StockAnalysis.scala
@@ -0,0 +1,25 @@
+package stocksproject
+
+import org.apache.spark.sql.SparkSession
+
+object StockAnalysis {
+	def main(args:Array[String]):Unit = {
+		val spark:SparkSession = SparkSession
+  		.builder()
+	  	.appName("Stock Project")
+      .master("yarn")
+//		  .config("spark.master", "yarn-cluster")
+//  		.config("spark.broadcast.compress", "false")
+//	  	.config("spark.option", "value")
+		.getOrCreate()
+
+		import spark.implicits._
+
+		val df =
+    spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://localhost:9000/stock-data/Google.csv")
+
+		df.printSchema
+		
+		spark.stop()
+	}
+}
